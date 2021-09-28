3e0f768a1da9139840ccba485f41219304b52a23
ywkim92 <akr1253@naver.com>
2021-09-28 18:53:07 +0900

    fix: Sampling without replacement

--- packages/Recommender_split.py
+++ packages/Recommender_split.py
@@ -15,9 +15,11 @@ def train_test_split_rec(data_input, user_vars, traning_only, test_for_training,
     for test in users_test:
         test_df = data[data[user_vars]==test]
         test_idx = test_df.index.tolist()
-        if (test_df.shape[0]*test_for_training) >= .5:
-            training_idx += rng.choice(test_idx, int(test_df.shape[0]*test_for_training)+1).tolist()
-        
+        n_samples = test_df.shape[0]
+        if (n_samples*test_for_training) >= .5:
+            rng = np.random.default_rng(random_state+n_samples)
+            training_idx += rng.choice(test_idx, int(n_samples*test_for_training)+1, replace=False).tolist()
+   
     train = data.loc[training_idx]
     test = data[data.index.isin(train.index)==False]
     
