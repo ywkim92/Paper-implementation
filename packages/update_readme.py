c16f52f924aa529dc786b78905dedd02a134e5b2
ywkim92 <akr1253@naver.com>
2022-03-24 15:40:43 +0900

    feat: Add code to add the nbviewer url of a jupyter notebook file that has already uploaded to gitbub to README.md

--- packages/update_readme.py
+++ packages/update_readme.py
@@ -1,5 +1,6 @@
-import re
 import os
+import re
+import copy
 import subprocess as sp
 
 def change_url():
@@ -9,24 +10,13 @@ def change_url():
     change_url_dict = dict(zip( [re.search(r'(?<=/)[^/\.]+\.ipynb',g).group(0) for g in gitfiles_nb] , gitfiles_nb))
     return change_url_dict
     
-def update_readme( file_list ):
-    readme_path = 'C:\\Users\\ywkim\\Github\\Paper implementation\\README-Copy1.md'
+def update_readme( ):
+    readme_path = 'C:\\Users\\ywkim\\Github\\Paper implementation\\README.md'
     github_url = 'https://github.com/ywkim92/Paper-implementation/blob/main/'
     notebook_viewer = 'https://nbviewer.org/github/ywkim92/Paper-implementation/blob/main/'
     
     change_url_dict_ = change_url()
-    
-    add_list = []
-    for name in file_list:
-        re_search = re.search(r'[\w\-/ %]+\.ipynb$', name)
-        if re_search is None: continue
-        else: 
-            re_search_word = re.search(r'[^/]+(?=\.ipynb)', re_search.group(0)).group(0)
-            re_search_url = notebook_viewer + re_search.group(0).replace(' ','%20')
-            add_str = '1. [{}]({})\n'.format( re_search_word.capitalize()  , re_search_url )
-            add_list.append(add_str)
-    print('newly added untracked files:',add_list)
-    
+    change_url_dict_copy = copy.deepcopy(change_url_dict_)
     
     lines = []
     with open(readme_path) as file:
@@ -37,10 +27,28 @@ def update_readme( file_list ):
                 f_keyword = re.search(r'[^)/]+\.ipynb$', f_cur_url).group(0)
                 if f_cur_url != change_url_dict_[f_keyword]:
                     print('* url changed', f_keyword)
+                    change_url_dict_copy.pop(f_keyword)
                     f = f.replace(f_cur_url, change_url_dict_[f_keyword])
             f = f.replace(github_url, notebook_viewer)
             
             lines.append(f)
+    
+    
+#   file_list = [g for g in sp.getoutput('git ls-files --others --exclude-standard').split('\n') if g.endswith('.ipynb')]
+    file_list = list(change_url_dict_copy.keys())
+    print(file_list)
+    add_list = []
+    for name in file_list:
+        re_search = re.search(r'[\w\-/ %]+\.ipynb$', name)
+        if re_search is None: continue
+        else: 
+            re_search_word = re.search(r'[^/]+(?=\.ipynb)', re_search.group(0)).group(0)
+            if re_search_word in change_url_dict_.values():
+                re_search_url = notebook_viewer + re_search.group(0).replace(' ','%20')
+                add_str = '1. [{}]({})\n'.format( re_search_word.capitalize()  , re_search_url )
+                add_list.append(add_str)
+    print('newly added untracked files:',add_list)
+    
     
     division_idx = lines.index('- - -  \n')
     lines_pre = lines[:division_idx+1]
@@ -56,7 +64,7 @@ def update_readme( file_list ):
         
         
         
+        
 if __name__=='__main__':
     os.chdir('C:\\Users\\ywkim\\Github\\Paper implementation')
-    file_list = input().split(', ')
-    update_readme( file_list )
+    update_readme( )
